7cd72ec72ef0b664cf48574400ae458dd96010b0
AishaSaleem110 <aishasalim.cs72@gmail.com>
2022-03-03 11:27:31 +0000

    iteration 3 all functions completed

--- Herdle_iteration3.hs
+++ Herdle_iteration3.hs
@@ -22,30 +22,33 @@ checkGuessAux :: [(Char, Status)] -> String -> String -> String -> Int-> [(Char,
 checkGuessAux acc [] _ _ _ = acc
 checkGuessAux acc _ [] _ _= acc
 checkGuessAux acc (x:xs) (y:ys) word itr | x==y = checkGuessAux (acc ++ [(x,Here)]) xs ys word $ itr+1
-                                         | x `member` word &&  isBefore x itr ( getCharHereIndices x acc) word  = checkGuessAux (acc ++ [(x,Elsewhere (Just Before))]) xs ys word $ itr+1
-                                         | x `member` word  = checkGuessAux (acc ++ [(x,Elsewhere (Just After))]) xs ys word $itr+1
+                                         | x `member` word &&  getGuessDirection x itr word == Just Before  = checkGuessAux (acc ++ [(x,Elsewhere (Just Before))]) xs ys word $ itr+1
+                                         | x `member` word && getGuessDirection x itr word == Just After = checkGuessAux (acc ++ [(x,Elsewhere (Just After))]) xs ys word $itr+1
                                          | otherwise = checkGuessAux (acc ++ [(x,Nowhere)]) xs ys word $ itr+1
 
+                                    
 getCharHereIndices:: Char -> [(Char, Status)]-> [Int]
 getCharHereIndices char statuses = 
     let (element, index) = unzip $ filter (\((a,b),y)->a== char && b == Here ) $ zip statuses [0..] 
     in index
 
-isBefore:: Char->Int->[Int]->String-> Bool
-isBefore char g_index here_indices word =  do
-    let directions = computeCharacterDirections char g_index here_indices word
-    let before = filter ( == Before) directions
+getGuessDirection:: Char->Int->String-> Maybe Direction
+getGuessDirection char g_index  word =  do
+    let directions = computeCharacterDirections char g_index word 
+    let before = filter ( == Before) directions 
     let after =  filter (== After) directions
-    (length before > 0 && length after > 0)
+    if (length before > 0 && length after > 0)
      || (length before > 0) 
+        then return Before
+    else if length after > 0 
+        then return After
+    else
+        Nothing
 
-
-computeCharacterDirections:: Char -> Int -> [Int]-> String -> [Direction]
-computeCharacterDirections char g_index here_indices word = do
-    let char_in_word_indices = getCharIndices char word 
-    let filtered_indices = filter (`notElem` here_indices) char_in_word_indices 
-    getDirections filtered_indices g_index 
-
+computeCharacterDirections:: Char -> Int -> String -> [Direction]
+computeCharacterDirections char g_index  word = do
+    let char_in_word_indices = getCharIndices char word     
+    getDirections char_in_word_indices g_index 
 
 
 getDirections:: [Int]-> Int -> [Direction]
@@ -62,22 +65,30 @@ getCharIndices char word =  let
     zipped = zip word [0..length word]  in
       let (chars,indices) = unzip $ filter (\(a,b)-> char==a ) zipped in indices
 
-
 getGuess :: Integer -> [Char]-> IO String
-getGuess 0 allowed_chars  = do
+getGuess = getGuessAux "" 
+
+getGuessAux :: [Char]->Integer -> [Char]-> IO String
+getGuessAux acc 0 allowed_chars  = do
     putStrLn ""
-    return []
-getGuess n allowed_chars= do
+    return acc
+getGuessAux acc allowed_tries allowed_chars= do
+   
     inp <-  getChar'
     let inp' = toLower inp
-    if inp' `member` allowed_chars then
+    if inp' == '.' then
+        (do putStr "\b \b" >> putStr "\b \b" >> putStr "\b \b"
+            getGuessAux (init acc) (allowed_tries+1) allowed_chars)
 
-        (do putChar ' '
-            xs <- getGuess (n - 1) allowed_chars
-            return (inp' : xs))
+    else if inp' `member` allowed_chars then
+        (do
+            putChar ' '
+            let temp = getGuessAux (acc ++ [inp'] ) (allowed_tries - 1) allowed_chars in temp           
+        )
+    
     else
         (do putStr "\b \b"
-            getGuess n allowed_chars)
+            getGuessAux acc allowed_tries allowed_chars)
 
 --helper function
 getUserInputStatus :: [(Char,Status)] -> [Status]
@@ -85,27 +96,33 @@ getUserInputStatus [] = []
 getUserInputStatus ((a,b):xs) = b:getUserInputStatus xs
 
 
-loop :: String -> [Char] -> Int -> IO ()
-loop _ allowed_chars 0 = do
+loop :: String -> [Char] -> Int -> Int-> IO ()
+loop _ allowed_chars 0 attempt= do
     putStrLn $ prompt Lose
-loop word allowed_chars n = do
-    putStrLn "Enter a guess of five letter word: "
-    --putStrLn allowed_chars
-    inp <- getGuess 5 allowed_chars
-    let char_status = checkGuess inp word
-    --putStrLn $ show char_status
-    let statuses = getUserInputStatus char_status
-    putStrLn $ showStatus statuses
-    let len = length $ filter (== Here) statuses
-    case len of
-        5 -> putStrLn $ prompt Win
-        _ ->let updated_chars = updateAvailable allowed_chars char_status in
-                 loop word updated_chars (n-1)
+loop word allowed_chars n attempt = do
+    putStrLn $ "Attempt " ++ show attempt 
+    putStr $ prompt Start ++ " "
+    choice <- getChar'
+    putStrLn ""
+    case choice of
+        'q'-> putStrLn $ prompt Quit
+        _ -> do
+        putStr $ prompt Guess
+        inp <- getGuess 5 allowed_chars
+        let char_status = checkGuess inp word
+    
+        let statuses = getUserInputStatus char_status
+        putStrLn $ leftMargin ++ showStatus statuses
+        let len = length $ filter (== Here) statuses
+        case len of
+            5 -> putStrLn $ prompt Win
+            _ ->let updated_chars = updateAvailable allowed_chars char_status in
+                 loop word updated_chars (n-1) $ attempt+1
 
 
 go :: String -> IO ()
 go inp = do
-     loop (map toLower inp) "abcdefghijklmnopqrstuvwxyz" 6
+     loop (map toLower inp) "abcdefghijklmnopqrstuvwxyz" 6 1
      return ()
 
 --helper function
@@ -134,5 +151,5 @@ leftMarginAux acc = do
     if length acc == length (prompt Start) then
      acc
     else
-        leftMarginAux (acc ++ "")
+        leftMarginAux (acc ++ " ")
 
